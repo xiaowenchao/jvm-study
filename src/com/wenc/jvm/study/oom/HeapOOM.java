cdad38463765f6ace5d6b1c127819b8bab5bcb0b
xiaowenchao <675478717@qq.com>
2021-01-10 18:09:06 +0800

    初始导入

---
+++ src/com/wenc/jvm/study/oom/HeapOOM.java
@@ -0,0 +1,21 @@
+package com.wenc.jvm.study.oom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*** 
+ * VM Args£º-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./hprof/
+ * @author wenchao
+ * */ 
+
+public class HeapOOM {
+	static class OOMObject {}
+	
+	public static void main(String[] args) {
+		List<OOMObject> list = new ArrayList<OOMObject>(); 
+		while (true) {
+			list.add(new OOMObject()); 
+		} 
+	}
+	
+}
